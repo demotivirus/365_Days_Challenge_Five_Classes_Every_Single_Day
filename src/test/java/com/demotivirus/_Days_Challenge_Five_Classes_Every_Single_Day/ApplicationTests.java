62538cd0864e544212e0f74685a2a84f2b19208b
demotivirus <demotivirus@gmail.com>
2020-12-23 00:17:55 +0300

    Day 1

---
+++ src/test/java/com/demotivirus/_Days_Challenge_Five_Classes_Every_Single_Day/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demotivirus._Days_Challenge_Five_Classes_Every_Single_Day;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
