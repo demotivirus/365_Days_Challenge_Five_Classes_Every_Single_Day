86eb9494012cb2b32ab716db0fc7d2af9686dda1
demotivirus <demotivirus@gmail.com>
2021-01-13 11:12:21 +0300

    Day 23: add PUT

--- src/main/java/com/demotivirus/Day_021/Book/BookController.java
+++ src/main/java/com/demotivirus/Day_021/Book/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +38,24 @@ public class BookController {
     @PostMapping
     public Book createBook(@RequestBody Book book) {
         return bookDao.save(book);
+    }
+
+    @PutMapping("{bookId}")
+    public Book updateBook(@RequestBody Book book, @PathVariable Integer bookId){
+        Book existsBook = bookDao.findById(bookId)
+                .orElseThrow(() -> new NotFoundException("Book not found " + bookId));
+        existsBook.setTitle(book.getTitle());
+        existsBook.setAuthor(book.getAuthor());
+        return bookDao.save(existsBook);
+    }
+
+    @PutMapping
+    public Book updateBook(@RequestBody Book book){
+        Book existsBook = bookDao.findById(book.getId())
+                .orElseThrow(() -> new NotFoundException("Book not found " + book.getId()));
+        existsBook.setTitle(book.getTitle());
+        existsBook.setAuthor(book.getAuthor());
+        return bookDao.save(existsBook);
     }
 
     @DeleteMapping("{bookId}")
