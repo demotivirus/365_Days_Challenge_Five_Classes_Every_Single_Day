1ab970c4c13e33d59817f5563dc2a4bb0ecfe932
demotivirus <demotivirus@gmail.com>
2021-01-19 23:16:28 +0300

    Day 29: JUnit5

---
+++ src/test/java/com/demotivirus/Day_029/MultyListTest.java
@@ -0,0 +1,28 @@
+package com.demotivirus.Day_029;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MultyListTest {
+    private MultyList multyList;
+
+    @BeforeEach
+    public void init(){
+        multyList = new MultyList();
+    }
+
+    @Test
+    public void testMultyList(){
+        List<String> list = new ArrayList<>(){{
+           add("String");
+           add("String 2");
+           add("String 3");
+        }};
+        multyList.setList(Collections.singletonList(list));
+        System.out.println(multyList);
+    }
+}
