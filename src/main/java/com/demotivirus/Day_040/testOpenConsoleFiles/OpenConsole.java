eff38dd07f588c79156c336c794bccdf62853411
demotivirus <demotivirus@gmail.com>
2021-01-30 23:49:28 +0300

    Day 40: Test nested classes & open files

--- src/main/java/com/demotivirus/Day_040/testOpenConsoleFiles/OpenConsole.java
+++ src/main/java/com/demotivirus/Day_040/testOpenConsoleFiles/OpenConsole.java
@@ -1,14 +1,14 @@
-package com.demotivirus.Day_040;
+package com.demotivirus.Day_040.testOpenConsoleFiles;
 
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
-public class OpenFile {
+public class OpenConsole {
     public static void main(String[] args) {
         String path = "..\\.\\365_Days_Challenge_Five_Classes_Every_Single_Day" +
                 "\\365_Days_Challenge_Five_Classes_Every_Single_Day" +
-                "\\src\\main\\java\\com\\demotivirus\\Day_040\\hello.txt";
+                "\\src\\main\\java\\com\\demotivirus\\Day_040\\dir.bat";
 
         File file = new File(path);
         Desktop desktop = Desktop.getDesktop();
--- src/main/java/com/demotivirus/Day_040/testOpenConsoleFiles/OpenFile.java
+++ src/main/java/com/demotivirus/Day_040/testOpenConsoleFiles/OpenFile.java
@@ -1,14 +1,14 @@
-package com.demotivirus.Day_040;
+package com.demotivirus.Day_040.testOpenConsoleFiles;
 
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
-public class OpenConsole {
+public class OpenFile {
     public static void main(String[] args) {
         String path = "..\\.\\365_Days_Challenge_Five_Classes_Every_Single_Day" +
                 "\\365_Days_Challenge_Five_Classes_Every_Single_Day" +
-                "\\src\\main\\java\\com\\demotivirus\\Day_040\\dir.bat";
+                "\\src\\main\\java\\com\\demotivirus\\Day_040\\hello.txt";
 
         File file = new File(path);
         Desktop desktop = Desktop.getDesktop();
