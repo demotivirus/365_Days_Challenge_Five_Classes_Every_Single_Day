4a8de3ea52a8068cf683dc0d4729e462c61f9749
demotivirus <demotivirus@gmail.com>
2021-02-07 00:17:55 +0300

    Day 47: Create dictionary db

--- src/main/java/com/demotivirus/Day_047_Dictionary/Dictionary.java
+++ src/main/java/com/demotivirus/Day_047_Dictionary/Dictionary.java
@@ -2,9 +2,8 @@ package com.demotivirus.Day_047_Dictionary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class Dictionary {
     public static void main(String[] args) {
         SpringApplication.run(Dictionary.class);
